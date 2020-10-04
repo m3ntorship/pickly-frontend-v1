36d3c038505b9385e52d92c703d04986d8cddb45
Ahmed Ayoub <great.nile@gmail.com>
2020-10-04 12:10:50 +0200

    integrate global styles with storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
+const { setupPostcss } = require('../config-webpack-postcss');
+
 module.exports = {
+  webpackFinal: config => {
+    return setupPostcss(config);
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import '../src/style/tailwind.css';
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
 };
