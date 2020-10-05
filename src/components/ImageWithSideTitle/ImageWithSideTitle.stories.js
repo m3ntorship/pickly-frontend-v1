46e955a872d9fb50efcbfe199e6980191a139a36
AlaaSaedy <alaa.saedy.eg@gmail.com>
2020-10-05 22:19:13 +0200

    - Restructuring the component to fit different cases. - Add optional background. - Use of Heading component and adding the required props.

--- src/components/ImageWithSideTitle/ImageWithSideTitle.stories.js
+++ src/components/ImageWithSideTitle/ImageWithSideTitle.stories.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import ImageWithSideTitle, { TITLE_COLOR } from './index';
+import { ImageWithSideTitle, BG_COLOR } from './index';
 
 export default {
   title: 'ImageWithSideTitle',
   component: ImageWithSideTitle,
   argTypes: {
-    titleColor: {
+    bgColor: {
       control: {
         type: 'select',
         options: [
-          TITLE_COLOR.BLUE,
-          TITLE_COLOR.RED,
-          TITLE_COLOR.LIGHTBLACK,
-          TITLE_COLOR.DAWNBLACK,
-          TITLE_COLOR.CLOUDGREY,
-          TITLE_COLOR.SUBMARINEGREY,
-          TITLE_COLOR.WHITEGREY,
-          TITLE_COLOR.SILVERGREY,
-          TITLE_COLOR.OFFWHITE,
-          TITLE_COLOR.GREEN,
-          TITLE_COLOR.WHITE,
-          TITLE_COLOR.BLACK
+          BG_COLOR.BLUE,
+          BG_COLOR.LIGHTBLACK,
+          BG_COLOR.DAWNBLACK,
+          BG_COLOR.CLOUDGREY,
+          BG_COLOR.SUBMARINEGREY,
+          BG_COLOR.WHITEGREY,
+          BG_COLOR.SILVERGREY,
+          BG_COLOR.OFFWHITE,
+          BG_COLOR.GREEN,
+          BG_COLOR.WHITE,
+          BG_COLOR.BLACK
         ]
       }
     }
@@ -33,6 +32,25 @@ export const ImgWithSideTitle = Template.bind({});
 ImgWithSideTitle.args = {
   imgUrl: 'https://svgur.com/i/QGG.svg',
   title: 'Schedule',
-  titleColor: TITLE_COLOR.BLUE,
   subTitle: 'simple subtitle here'
+};
+
+export const ImgWithSideDarkTitle = Template.bind({});
+ImgWithSideDarkTitle.args = {
+  imgUrl: 'https://svgur.com/i/QGG.svg',
+  title: 'Multiple images',
+  titleColor: BG_COLOR.LIGHTBLACK,
+  titleFontWeight: 'semibold',
+  titleAlign: 'left'
+};
+
+export const ImgWithSideTitleWithBg = Template.bind({});
+ImgWithSideTitleWithBg.args = {
+  imgUrl: 'https://svgur.com/i/QGG.svg',
+  title: 'Anonymous',
+  titleColor: BG_COLOR.BLACK,
+  subTitle: 'simple subtitle here',
+  titleFontWeight: 'semibold',
+  titleAlign: 'left',
+  bgColor: BG_COLOR.SILVERGREY
 };
--- src/components/ImageWithSideTitle/index.jsx
+++ src/components/ImageWithSideTitle/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Heading } from '../Heading/index';
 import cn from 'classnames';
 
-export const TITLE_COLOR = {
+export const BG_COLOR = {
   BLUE: 'blue',
   RED: 'red',
   LIGHTBLACK: 'lightblack',
@@ -16,34 +17,51 @@ export const TITLE_COLOR = {
   BLACK: 'black'
 };
 
-const ImageWithSideText = ({ imgUrl, title, titleColor, subTitle }) => {
+export const ImageWithSideTitle = ({
+  imgUrl,
+  title,
+  subTitle,
+  titleColor,
+  bgColor,
+  titleFontWeight,
+  titleAlign
+}) => {
   return (
-    <div className="flex justify-around rounded py-2">
-      <img src={imgUrl} alt="" />
+    <button className="p-5">
+      <div className="flex justify-around items-center rounded py-3">
+        <div
+          className={cn('p-3 rounded-full', {
+            'bg-c100': bgColor === BG_COLOR.BLUE,
+            'bg-c200': bgColor === BG_COLOR.RED,
+            'bg-c300': bgColor === BG_COLOR.LIGHTBLACK,
+            'bg-c400': bgColor === BG_COLOR.DAWNBLACK,
+            'bg-c500': bgColor === BG_COLOR.CLOUDGREY,
+            'bg-c600': bgColor === BG_COLOR.SUBMARINEGREY,
+            'bg-c700': bgColor === BG_COLOR.WHITEGREY,
+            'bg-c800': bgColor === BG_COLOR.SILVERGREY,
+            'bg-c900': bgColor === BG_COLOR.OFFWHITE,
+            'bg-c1000': bgColor === BG_COLOR.GREEN,
+            'bg-white': bgColor === BG_COLOR.WHITE,
+            'bg-black': bgColor === BG_COLOR.BLACK
+          })}
+        >
+          <img src={imgUrl} alt="" />
+        </div>
 
-      <p
-        className={cn({
-          'text-c100': titleColor === TITLE_COLOR.BLUE,
-          'text-c200': titleColor === TITLE_COLOR.RED,
-          'text-c300': titleColor === TITLE_COLOR.LIGHTBLACK,
-          'text-c400': titleColor === TITLE_COLOR.DAWNBLACK,
-          'text-c500': titleColor === TITLE_COLOR.CLOUDGREY,
-          'text-c600': titleColor === TITLE_COLOR.SUBMARINEGREY,
-          'text-c700': titleColor === TITLE_COLOR.WHITEGREY,
-          'text-c800': titleColor === TITLE_COLOR.SILVERGREY,
-          'text-c900': titleColor === TITLE_COLOR.OFFWHITE,
-          'text-c1000': titleColor === TITLE_COLOR.GREEN,
-          'text-white': titleColor === TITLE_COLOR.WHITE,
-          'text-black': titleColor === TITLE_COLOR.BLACK
-        })}
-      >
-        {title}
-        {subTitle && (
-          <span className="block mt-1 text-c500 text-xs">{subTitle}</span>
-        )}
-      </p>
-    </div>
+        <div className="ml-5">
+          <Heading
+            children={title}
+            textAlign={titleAlign}
+            textColor={titleColor}
+            fontWeight={titleFontWeight}
+          />
+          {subTitle && (
+            <span className="block mt-3 text-c500 text-xs text-left">
+              {subTitle}
+            </span>
+          )}
+        </div>
+      </div>
+    </button>
   );
 };
-
-export default ImageWithSideText;
