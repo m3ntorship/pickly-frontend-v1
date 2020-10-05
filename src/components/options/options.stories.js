d5fe30eadfdd88272a03c738e58a98908fe1f557
MAlngaawy <m.alngaawy@gmail.com>
2020-10-05 16:45:20 +0200

    add the popup position and appear on options to storybook and create an argeTypes to controll them

--- src/components/options/options.stories.js
+++ src/components/options/options.stories.js
@@ -1,15 +1,47 @@
 import React from 'react';
 
-import { OptionsBtn } from './';
+import PopUp from './';
+
+const APPEAR_ON_OPTIONS = ['click', 'hover', 'right-click'];
+
+const POSITION_TYPES = [
+  'top left',
+  'top center',
+  'top right',
+  'right top',
+  'right center',
+  'right bottom',
+  'bottom left',
+  'bottom center',
+  'bottom right',
+  'left top',
+  'left center',
+  'left bottom',
+  'center center'
+];
 
 export default {
-  title: 'components/options button',
-  component: OptionsBtn
+  title: 'components/Pop up',
+  component: PopUp,
+  argTypes: {
+    appearOn: {
+      control: {
+        type: 'select',
+        options: [...APPEAR_ON_OPTIONS]
+      }
+    },
+    position: {
+      control: {
+        type: 'select',
+        options: [...POSITION_TYPES]
+      }
+    }
+  }
 };
 
-const Template = args => <OptionsBtn {...args} />;
+const Template = args => <PopUp {...args} />;
 
-export const unselected = Template.bind({});
-unselected.args = {
+export const test = Template.bind({});
+test.args = {
   options: ['option1', 'option2', 'option3']
 };
