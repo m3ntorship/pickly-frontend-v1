44e8bee3f0822f0b195618616abdda6d6ef77743
mina bessada <mina@wizholding.com>
2020-10-06 12:11:51 +0200

    create story

--- src/components/Paragraph/Paragraph.stories.js
+++ src/components/Paragraph/Paragraph.stories.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Heading, HEADING_OPTIONS } from './index';
+import { PARAGRAPH, PARAGRAPH_OPTIONS } from './index';
 
 export default {
-  title: 'Reusable Heading',
-  component: Heading,
+  title: 'Reusable PARAGRAPH',
+  component: PARAGRAPH,
   argTypes: {
     fontSize: {
       control: {
         type: 'select',
         options: [
-          HEADING_OPTIONS.FONT_SIZE.XLARGE,
-          HEADING_OPTIONS.FONT_SIZE.LARGE,
-          HEADING_OPTIONS.FONT_SIZE.MEDIUM,
-          HEADING_OPTIONS.FONT_SIZE.SMALL,
-          HEADING_OPTIONS.FONT_SIZE.XSMALL
+          PARAGRAPH_OPTIONS.FONT_SIZE.XLARGE,
+          PARAGRAPH_OPTIONS.FONT_SIZE.LARGE,
+          PARAGRAPH_OPTIONS.FONT_SIZE.MEDIUM,
+          PARAGRAPH_OPTIONS.FONT_SIZE.SMALL,
+          PARAGRAPH_OPTIONS.FONT_SIZE.XSMALL
         ]
       }
     },
@@ -21,10 +21,10 @@ export default {
       control: {
         type: 'select',
         options: [
-          HEADING_OPTIONS.FONT_WEIGHT.REGULAR,
-          HEADING_OPTIONS.FONT_WEIGHT.MEDIUM,
-          HEADING_OPTIONS.FONT_WEIGHT.SEMIBOLD,
-          HEADING_OPTIONS.FONT_WEIGHT.BOLD
+          PARAGRAPH_OPTIONS.FONT_WEIGHT.REGULAR,
+          PARAGRAPH_OPTIONS.FONT_WEIGHT.MEDIUM,
+          PARAGRAPH_OPTIONS.FONT_WEIGHT.SEMIBOLD,
+          PARAGRAPH_OPTIONS.FONT_WEIGHT.BOLD
         ]
       }
     },
@@ -32,9 +32,9 @@ export default {
       control: {
         type: 'select',
         options: [
-          HEADING_OPTIONS.LINE_HEIGHT.TIGHT,
-          HEADING_OPTIONS.LINE_HEIGHT.NORMAL,
-          HEADING_OPTIONS.LINE_HEIGHT.LOOSE
+          PARAGRAPH_OPTIONS.LINE_HEIGHT.TIGHT,
+          PARAGRAPH_OPTIONS.LINE_HEIGHT.NORMAL,
+          PARAGRAPH_OPTIONS.LINE_HEIGHT.LOOSE
         ]
       }
     },
@@ -43,54 +43,36 @@ export default {
       control: {
         type: 'select',
         options: [
-          HEADING_OPTIONS.FONT_COLOR.BLUE,
-          HEADING_OPTIONS.FONT_COLOR.LIGHTBLACK,
-          HEADING_OPTIONS.FONT_COLOR.DAWNBLACK,
-          HEADING_OPTIONS.FONT_COLOR.CLOUDGREY,
-          HEADING_OPTIONS.FONT_COLOR.SUBMARINEGREY,
-          HEADING_OPTIONS.FONT_COLOR.WHITEGREY,
-          HEADING_OPTIONS.FONT_COLOR.SILVERGREY,
-          HEADING_OPTIONS.FONT_COLOR.OFFWHITE,
-          HEADING_OPTIONS.FONT_COLOR.GREEN,
-          HEADING_OPTIONS.FONT_COLOR.WHITE,
-          HEADING_OPTIONS.FONT_COLOR.BLACK
+          PARAGRAPH_OPTIONS.FONT_COLOR.BLUE,
+          PARAGRAPH_OPTIONS.FONT_COLOR.LIGHTBLACK,
+          PARAGRAPH_OPTIONS.FONT_COLOR.DAWNBLACK,
+          PARAGRAPH_OPTIONS.FONT_COLOR.CLOUDGREY,
+          PARAGRAPH_OPTIONS.FONT_COLOR.SUBMARINEGREY,
+          PARAGRAPH_OPTIONS.FONT_COLOR.WHITEGREY,
+          PARAGRAPH_OPTIONS.FONT_COLOR.SILVERGREY,
+          PARAGRAPH_OPTIONS.FONT_COLOR.OFFWHITE,
+          PARAGRAPH_OPTIONS.FONT_COLOR.GREEN,
+          PARAGRAPH_OPTIONS.FONT_COLOR.WHITE,
+          PARAGRAPH_OPTIONS.FONT_COLOR.BLACK
         ]
       }
     }
   }
 };
 
-const Template = args => <Heading {...args} />;
+const Template = args => <PARAGRAPH {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
   children: 'Anonymous',
-  fontSize: HEADING_OPTIONS.FONT_SIZE.LARGE,
-  textAlign: HEADING_OPTIONS.TEXT_ALIGN.RIGHT,
+  fontSize: PARAGRAPH_OPTIONS.FONT_SIZE.LARGE,
+
   lessOpacity: true,
-  textColor: HEADING_OPTIONS.FONT_COLOR.RED
+  textColor: PARAGRAPH_OPTIONS.FONT_COLOR.RED
 };
 export const Secondary = Template.bind({});
 Secondary.args = {
-  children: 'Secondary Heading',
-  textColor: HEADING_OPTIONS.FONT_COLOR.LIGHT,
-  fontSize: HEADING_OPTIONS.FONT_SIZE.MEDIUM,
-
-  textAlign: HEADING_OPTIONS.TEXT_ALIGN.LEFT
-};
-export const Small = Template.bind({});
-Small.args = {
-  children: 'Small Heading',
-  textColor: HEADING_OPTIONS.FONT_COLOR.SOLID,
-  fontSize: HEADING_OPTIONS.FONT_SIZE.SMALL,
-  textAlign: HEADING_OPTIONS.CENTER
-};
-export const Anonymous = Template.bind({});
-Anonymous.args = {
-  children: 'Anonymous Heading',
-  textColor: HEADING_OPTIONS.FONT_COLOR.SOLID,
-  fontSize: HEADING_OPTIONS.FONT_SIZE.SMALL,
-  fontWeight: HEADING_OPTIONS.FONT_WEIGHT.SEMIBOLD,
-  textAlign: HEADING_OPTIONS.CENTER,
-  lineHeight: HEADING_OPTIONS.LINE_HEIGHT.LOOSE
+  children: 'Secondary PARAGRAPH',
+  textColor: PARAGRAPH_OPTIONS.FONT_COLOR.LIGHT,
+  fontSize: PARAGRAPH_OPTIONS.FONT_SIZE.MEDIUM
 };
