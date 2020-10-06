c1b19da1ff041b82b721451ef66462d1059d9650
mina bessada <mina@wizholding.com>
2020-10-06 12:00:36 +0200

    UPDATE

--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 
-export const HEADING_OPTIONS = {
+export const PARAGRAPH_OPTIONS = {
   FONT_SIZE: {
     XSMALL: 'xs',
     SMALL: 'sm',
@@ -47,7 +47,7 @@ export const HEADING_OPTIONS = {
   }
 };
 
-export const Heading = ({
+export const PARAGRAPH = ({
   children,
   className,
   fontSize,
@@ -60,42 +60,42 @@ export const Heading = ({
   const classes = cn(
     {
       // FONT SIZE
-      'text-base': !fontSize || fontSize === HEADING_OPTIONS.FONT_SIZE.BASE,
-      'text-xs': fontSize === HEADING_OPTIONS.FONT_SIZE.XSMALL,
-      'text-sm': fontSize === HEADING_OPTIONS.FONT_SIZE.SMALL,
-      'text-md': fontSize === HEADING_OPTIONS.FONT_SIZE.MEDIUM,
-      'text-lg': fontSize === HEADING_OPTIONS.FONT_SIZE.LARGE,
-      'text-xlg': fontSize === HEADING_OPTIONS.FONT_SIZE.XLARGE,
+      'text-base': !fontSize || fontSize === PARAGRAPH_OPTIONS.FONT_SIZE.BASE,
+      'text-xs': fontSize === PARAGRAPH_OPTIONS.FONT_SIZE.XSMALL,
+      'text-sm': fontSize === PARAGRAPH_OPTIONS.FONT_SIZE.SMALL,
+      'text-md': fontSize === PARAGRAPH_OPTIONS.FONT_SIZE.MEDIUM,
+      'text-lg': fontSize === PARAGRAPH_OPTIONS.FONT_SIZE.LARGE,
+      'text-xlg': fontSize === PARAGRAPH_OPTIONS.FONT_SIZE.XLARGE,
 
       // FONT WEIGHT
       'font-regular':
-        !fontWeight || fontWeight === HEADING_OPTIONS.FONT_WEIGHT.REGULAR,
-      'font-medium': fontWeight === HEADING_OPTIONS.FONT_WEIGHT.MEDIUM,
-      'font-semibold': fontWeight === HEADING_OPTIONS.FONT_WEIGHT.SEMIBOLD,
-      'font-bold': fontWeight === HEADING_OPTIONS.FONT_WEIGHT.BOLD,
+        !fontWeight || fontWeight === PARAGRAPH_OPTIONS.FONT_WEIGHT.REGULAR,
+      'font-medium': fontWeight === PARAGRAPH_OPTIONS.FONT_WEIGHT.MEDIUM,
+      'font-semibold': fontWeight === PARAGRAPH_OPTIONS.FONT_WEIGHT.SEMIBOLD,
+      'font-bold': fontWeight === PARAGRAPH_OPTIONS.FONT_WEIGHT.BOLD,
 
       // FONT COLOR
-      'text-c100': textColor === HEADING_OPTIONS.FONT_COLOR.BLUE,
-      'text-c200': textColor === HEADING_OPTIONS.FONT_COLOR.RED,
-      'text-c300': textColor === HEADING_OPTIONS.FONT_COLOR.LIGHTBLACK,
-      'text-c400': textColor === HEADING_OPTIONS.FONT_COLOR.DAWNBLACK,
-      'text-c500': textColor === HEADING_OPTIONS.FONT_COLOR.CLOUDGREY,
-      'text-c600': textColor === HEADING_OPTIONS.FONT_COLOR.SUBMARINEGREY,
-      'text-c700': textColor === HEADING_OPTIONS.FONT_COLOR.WHITEGREY,
-      'text-c800': textColor === HEADING_OPTIONS.FONT_COLOR.SILVERGREY,
-      'text-c900': textColor === HEADING_OPTIONS.FONT_COLOR.OFFWHITE,
-      'text-c1000': textColor === HEADING_OPTIONS.FONT_COLOR.GREEN,
-      'text-white': textColor === HEADING_OPTIONS.FONT_COLOR.WHITE,
-      'text-black': textColor === HEADING_OPTIONS.FONT_COLOR.BLACK,
+      'text-c100': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.BLUE,
+      'text-c200': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.RED,
+      'text-c300': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.LIGHTBLACK,
+      'text-c400': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.DAWNBLACK,
+      'text-c500': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.CLOUDGREY,
+      'text-c600': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.SUBMARINEGREY,
+      'text-c700': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.WHITEGREY,
+      'text-c800': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.SILVERGREY,
+      'text-c900': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.OFFWHITE,
+      'text-c1000': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.GREEN,
+      'text-white': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.WHITE,
+      'text-black': textColor === PARAGRAPH_OPTIONS.FONT_COLOR.BLACK,
 
       // FONT OPACITY
       'text-opacity-50': lessOpacity,
 
       // LINE HEIGHT
       'leading-4':
-        !lineHeight || lineHeight === HEADING_OPTIONS.LINE_HEIGHT.NORMAL,
-      'leading-3': lineHeight === HEADING_OPTIONS.LINE_HEIGHT.TIGHT,
-      'leading-5': lineHeight === HEADING_OPTIONS.LINE_HEIGHT.LOOSE
+        !lineHeight || lineHeight === PARAGRAPH_OPTIONS.LINE_HEIGHT.NORMAL,
+      'leading-3': lineHeight === PARAGRAPH_OPTIONS.LINE_HEIGHT.TIGHT,
+      'leading-5': lineHeight === PARAGRAPH_OPTIONS.LINE_HEIGHT.LOOSE
     },
     className
   );
@@ -103,6 +103,6 @@ export const Heading = ({
   return <>{React.createElement(as, { className: classes }, children)}</>;
 };
 
-Heading.propTypes = {
+PARAGRAPH.propTypes = {
   lessOpacity: PropTypes.bool
 };
