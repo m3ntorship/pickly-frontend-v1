266cd0e1d12eebad7aa1d684b578c046a2ed19e4
AlaaSaedy <alaa.saedy.eg@gmail.com>
2020-10-06 15:23:34 +0200

    - remove bg color object which became unwanted. - make optional to pass image or icon. - change img size either it's image or icon. - passing static props to heading component to match design cases.

--- src/components/ImageWithSideTitle/ImageWithSideTitle.stories.js
+++ src/components/ImageWithSideTitle/ImageWithSideTitle.stories.js
@@ -1,56 +1,38 @@
 import React from 'react';
-import { ImageWithSideTitle, BG_COLOR } from './index';
+import ImageWithSideTitle from './index';
 
 export default {
   title: 'ImageWithSideTitle',
-  component: ImageWithSideTitle,
-  argTypes: {
-    bgColor: {
-      control: {
-        type: 'select',
-        options: [
-          BG_COLOR.BLUE,
-          BG_COLOR.LIGHTBLACK,
-          BG_COLOR.DAWNBLACK,
-          BG_COLOR.CLOUDGREY,
-          BG_COLOR.SUBMARINEGREY,
-          BG_COLOR.WHITEGREY,
-          BG_COLOR.SILVERGREY,
-          BG_COLOR.OFFWHITE,
-          BG_COLOR.GREEN,
-          BG_COLOR.WHITE,
-          BG_COLOR.BLACK
-        ]
-      }
-    }
-  }
+  component: ImageWithSideTitle
 };
 
 const Template = args => <ImageWithSideTitle {...args} />;
 
 export const ImgWithSideTitle = Template.bind({});
 ImgWithSideTitle.args = {
-  imgUrl: 'https://svgur.com/i/QGG.svg',
+  iconURL: 'https://svgur.com/i/QGG.svg',
   title: 'Schedule',
   subTitle: 'simple subtitle here'
 };
 
 export const ImgWithSideDarkTitle = Template.bind({});
 ImgWithSideDarkTitle.args = {
-  imgUrl: 'https://svgur.com/i/QGG.svg',
-  title: 'Multiple images',
-  titleColor: BG_COLOR.LIGHTBLACK,
-  titleFontWeight: 'semibold',
-  titleAlign: 'left'
+  iconURL: 'http://svgur.com/i/QHo.svg',
+  title: 'Multiple images'
 };
 
 export const ImgWithSideTitleWithBg = Template.bind({});
 ImgWithSideTitleWithBg.args = {
-  imgUrl: 'https://svgur.com/i/QGG.svg',
+  imgURL:
+    'https://images.unsplash.com/photo-1555445091-5a8b655e8a4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
   title: 'Anonymous',
-  titleColor: BG_COLOR.BLACK,
   subTitle: 'simple subtitle here',
-  titleFontWeight: 'semibold',
-  titleAlign: 'left',
-  bgColor: BG_COLOR.SILVERGREY
+  isImgRounded: true
+};
+
+export const ImgWithSideTitleWithoutImg = Template.bind({});
+ImgWithSideTitleWithoutImg.args = {
+  iconURL: 'http://svgur.com/i/QH6.svg',
+  title: 'Anonymous',
+  subTitle: 'simple subtitle here'
 };
--- src/components/ImageWithSideTitle/index.jsx
+++ src/components/ImageWithSideTitle/index.jsx
@@ -1,61 +1,33 @@
 import React from 'react';
 import { Heading } from '../Heading/index';
-import cn from 'classnames';
 
-export const BG_COLOR = {
-  BLUE: 'blue',
-  RED: 'red',
-  LIGHTBLACK: 'lightblack',
-  DAWNBLACK: 'dawnblack',
-  CLOUDGREY: 'cloudgrey',
-  SUBMARINEGREY: 'submarinegrey',
-  WHITEGREY: 'whitegray',
-  SILVERGREY: 'silvergrey',
-  OFFWHITE: 'offwhite',
-  GREEN: 'green',
-  WHITE: 'white',
-  BLACK: 'black'
-};
-
-export const ImageWithSideTitle = ({
-  imgUrl,
+const ImageWithSideTitle = ({
+  imgURL,
+  iconURL,
   title,
   subTitle,
-  titleColor,
-  bgColor,
-  titleFontWeight,
-  titleAlign
+  titleColor
 }) => {
   return (
     <div className="flex justify-around items-center rounded py-3">
-      <div
-        className={cn('p-3 rounded-full', {
-          'bg-c100': bgColor === BG_COLOR.BLUE,
-          'bg-c200': bgColor === BG_COLOR.RED,
-          'bg-c300': bgColor === BG_COLOR.LIGHTBLACK,
-          'bg-c400': bgColor === BG_COLOR.DAWNBLACK,
-          'bg-c500': bgColor === BG_COLOR.CLOUDGREY,
-          'bg-c600': bgColor === BG_COLOR.SUBMARINEGREY,
-          'bg-c700': bgColor === BG_COLOR.WHITEGREY,
-          'bg-c800': bgColor === BG_COLOR.SILVERGREY,
-          'bg-c900': bgColor === BG_COLOR.OFFWHITE,
-          'bg-c1000': bgColor === BG_COLOR.GREEN,
-          'bg-white': bgColor === BG_COLOR.WHITE,
-          'bg-black': bgColor === BG_COLOR.BLACK
-        })}
-      >
-        <img src={imgUrl} alt="" />
+      <div className="rounded-full h-10 w-10 flex items-center justify-center">
+        <img
+          className={` ${imgURL ? 'rounded-full h-full w-full' : 'h-4 w-4'}`}
+          src={imgURL || iconURL}
+          alt=""
+        />
       </div>
 
       <div className="ml-5">
         <Heading
+          fontSize="sm"
           children={title}
-          textAlign={titleAlign}
+          textAlign="left"
           textColor={titleColor}
-          fontWeight={titleFontWeight}
+          fontWeight="semibold"
         />
         {subTitle && (
-          <span className="block mt-3 text-c500 text-xs text-left">
+          <span className="block mt-1 text-c500 text-xs text-left">
             {subTitle}
           </span>
         )}
@@ -63,3 +35,5 @@ export const ImageWithSideTitle = ({
     </div>
   );
 };
+
+export default ImageWithSideTitle;
