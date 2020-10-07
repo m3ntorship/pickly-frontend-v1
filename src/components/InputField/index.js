70d7411ff3c4c646475864745a29f8ac12936948
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-10-07 18:44:06 +0200

    build the component and style it

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
-
-export const InputField = () => {
-  return <div></div>;
+import { uploadigProgress } from '../Uploading-progress/uploadingProgress.stories';
+import { UploadingProgress } from './../Uploading-progress';
+export const InputField = ({ imageURL, placeHold, progress }) => {
+  return (
+    <div className="w-full rounded-lg border border-solid border-c700 flex flex-row p-4">
+      <img src={imageURL} alt="" className="w-10 h-10 rounded-full " />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder={placeHold}
+        className="ml-4 text-c500 text-base font-primary font-regular leading-6 w-4/5 "
+      />
+      <uploadigProgress
+        className="w-10 h-10 to z-10 bg-c200 "
+        progress={progress}
+      />
+    </div>
+  );
 };
