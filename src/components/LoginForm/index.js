ab137f0600bea0d5ff01b56184344d2798f6b057
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-10-08 23:56:32 +0200

    set google authentacation

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import background1 from './backgrounds/background1.png';
 import background2 from './backgrounds/background2.png';
 
@@ -6,6 +6,48 @@ import fire from './fire';
 import firebase from 'firebase';
 
 export const LoginForm = () => {
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    fire
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        // var token = result.credential.accessToken;
+        // The signed-in user info.
+        // var user = result.user;
+        // console.log(user);
+        // ...
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+        console.log(user.displayName);
+        console.log(user);
+      } else {
+        setUser(null);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+        console.log(user.displayName);
+        console.log(user);
+      } else {
+        setUser(null);
+      }
+    });
+  });
+
   return (
     <div className="w-full flex h-screen relative items-center justify-center ">
       <div
@@ -29,7 +71,12 @@ export const LoginForm = () => {
       <div className=" w-10/12 h-64 lg:w-lg lg:h-lg bg-white absolute z-30 rounded-xlg flex pt-24 ">
         <div className="h-full mx-auto ">
           <h1 className="font-xbold text-xxlg ">Cherrime</h1>
-          <button className="font-secondary font-bold text-base text-center bg-c200  text-white py-4 px-40 mt-32 rounded-lg" >Login with email</button>
+          <button
+            className="font-secondary font-bold text-base text-center bg-c200  text-white py-4 px-40 mt-32 rounded-lg"
+            onClick={login}
+          >
+            Login with email
+          </button>
         </div>
       </div>
     </div>
