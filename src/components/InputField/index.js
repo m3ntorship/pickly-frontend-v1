90f9bdd85fae8e91469a79438125a2c3cd6be375
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-10-08 14:34:41 +0200

    adjust text area and set post text state

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UploadingProgress } from './../Uploading-progress';
 export const InputField = ({ imageURL, progress }) => {
+  const [postText, setPostText] = useState(null);
+
+  const handleOnchange = e => {
+    const value = e.target.value;
+    setPostText(value);
+    console.log(postText);
+  };
   return (
-    <div className=" w-full rounded-lg border border-solid border-c700 flex items-center p-4  ">
-      <img src={imageURL} alt="" className="w-8 h-8 rounded-full" />
-      <input
-        type="text"
-        name=""
+    <div className=" w-full h-20 rounded-lg border border-solid border-c700 flex items-center p-4 z-10 ">
+      <img src={imageURL} alt="" className="w-8 h-8 rounded-full " />
+      <textarea
+        name="postText"
         id=""
         placeholder={'What’s on your mind?'}
-        className="mx-4 flex-auto min-w-0 text-c500 text-base font-primary font-regular leading-6 tracking-tighter md:tracking-wider"
-      />
+        onChange={handleOnchange}
+        className="mx-4 z-0 h-16 md:h-10 md:mt-2  flex-auto min-w-0 text-c500 text-base font-primary font-regular leading-6 tracking-tighter md:tracking-wider resize-none"
+      ></textarea>
       <UploadingProgress className="right-0" progress={progress} />
     </div>
   );
