996323de908c9046cb2f73e45d1fabab78862d30
MAlngaawy <m.alngaawy@gmail.com>
2020-10-08 15:46:26 +0200

    initialize navabr storybook

---
+++ src/components/NavbarSection/navbar.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navbar } from './';
+
+export default {
+  title: 'sections/Navbar',
+  component: Navbar
+};
+
+export const NavbarShowcase = () => {
+  return <Navbar />;
+};
