cbce0fc02c30eed0b22d10a6c18a59662b9eb13f
MAlngaawy <m.alngaawy@gmail.com>
2020-10-09 11:22:51 +0200

    create pages folder and home page file & use react route in app file & create a component for every page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { TestComponent } from './components/testComponent';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar } from './components/NavbarSection';
+import { Home } from './pages/home.jsx';
 
 function App() {
   return (
-    <div>
-      <TestComponent />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/friends" component={Friends} />
+          <Route path="/notifications" component={Notifications} />
+          <Route path="/profile" component={profile} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+const Friends = () => {
+  return (
+    <div className="h-screen bg-c800">
+      <h1>Hello, Friends</h1>
+    </div>
+  );
+};
+
+const Notifications = () => {
+  return (
+    <div className="h-screen bg-c800">
+      <h1>Hello, Alerts</h1>
+    </div>
+  );
+};
+
+const profile = () => {
+  return (
+    <div className="h-screen bg-c800">
+      <h1>Hello, Profile</h1>
+    </div>
+  );
+};
