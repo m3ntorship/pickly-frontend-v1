dfe869f7e1132394cda906370fb71629290789f9
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-10-10 15:39:15 +0200

    context api

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -51,36 +51,36 @@ export const LoginForm = () => {
   return (
     <div className="w-full flex h-screen relative items-center justify-center ">
       <UserContext.Provider value={{ user }}>
-      <div
-        className="z-10 w-full absolute min-h-full"
-        style={{
-          backgroundColor: 'rgba(0, 0, 0, 0.3)'
-        }}
-      ></div>
-      <div
-        className="h-full w-1/2 bg-no-repeat bg-cover bg-center z-0 "
-        style={{
-          backgroundImage: `url(${background1})`
-        }}
-      ></div>
-      <div
-        className="h-full w-1/2 bg-no-repeat bg-cover bg-c100_op-10 z-0"
-        style={{
-          backgroundImage: `url(${background2})`
-        }}
-      ></div>
-      <div className="bg-white absolute z-30 rounded-lg flex w-11/12 pt-8 pb-32 flex-col pl-6 md:pb-40 lg:w-lg lg:rounded-xlg lg:pl-24 lg:pb-40 ">
-        <div className="">
-          <h1 className="font-xbold text-xxlg ">Pickly</h1>
-          <button
-            className="font-secondary font-bold text-base text-center bg-c200  text-white rounded-md mt-20 py-4 px-10 lg:w-3/4 lg:mx-auto lg:rounded-lg"
-            onClick={login}
-          >
-            Login with Google
-          </button>
+        <div
+          className="z-10 w-full absolute min-h-full"
+          style={{
+            backgroundColor: 'rgba(0, 0, 0, 0.3)'
+          }}
+        ></div>
+        <div
+          className="h-full w-1/2 bg-no-repeat bg-cover bg-center z-0 "
+          style={{
+            backgroundImage: `url(${background1})`
+          }}
+        ></div>
+        <div
+          className="h-full w-1/2 bg-no-repeat bg-cover bg-c100_op-10 z-0"
+          style={{
+            backgroundImage: `url(${background2})`
+          }}
+        ></div>
+        <div className="bg-white absolute z-30 rounded-lg flex w-11/12 pt-8 pb-32 flex-col pl-6 md:pb-40 lg:w-lg lg:rounded-xlg lg:pl-24 lg:pb-40 ">
+          <div className="">
+            <h1 className="font-xbold text-xxlg ">Pickly</h1>
+            <button
+              className="font-secondary font-bold text-base text-center bg-c200  text-white rounded-md mt-20 py-4 px-10 lg:w-3/4 lg:mx-auto lg:rounded-lg"
+              onClick={login}
+            >
+              Login with Google
+            </button>
+          </div>
         </div>
-      </div>
-      </UserContext.Provider> 
+      </UserContext.Provider>
     </div>
   );
 };
