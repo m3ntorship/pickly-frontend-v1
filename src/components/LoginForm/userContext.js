141abe4ace4a5c136afed4673976d0022c32e4f3
abdelrhmanali29 <abdelrahman.ali.sayed28@gmail.com>
2020-10-11 01:32:55 +0200

    create context for user

---
+++ src/components/LoginForm/userContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useEffect } from 'react';
+import fire from './fire';
+
+export const UserContext = createContext(null);
+
+export const UserContextProvider = props => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+        console.log(user.displayName + 'from context');
+      } else {
+        setUser(null);
+      }
+    });
+  });
+
+  return (
+    <UserContext.Provider value={user}>{props.children}</UserContext.Provider>
+  );
+};
