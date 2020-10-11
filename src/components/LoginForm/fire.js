3deeaee0e28ea2ada20bd9d6d934d4feeda9404f
Ahmed Ayoub <great.nile@gmail.com>
2020-10-11 21:49:42 +0200

    use m3ntorship-pickly firebase app and connect to the backend with idToken

--- src/components/LoginForm/fire.js
+++ src/components/LoginForm/fire.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB7xdKm_It9ZqWxioKJYqmOyjDAtfRVRms',
-  authDomain: 'react-image-upload-18373.firebaseapp.com',
-  databaseURL: 'https://react-image-upload-18373.firebaseio.com',
-  projectId: 'react-image-upload-18373',
-  storageBucket: 'react-image-upload-18373.appspot.com',
-  messagingSenderId: '380656951111',
-  appId: '1:380656951111:web:f20ccc0fb3a2ca19f5f662'
+  apiKey: 'AIzaSyCcpWjIBgQzIc5y404dOqtm8QkXp-1kl0Y',
+  authDomain: 'm3ntorship-pickly.firebaseapp.com',
+  databaseURL: 'https://m3ntorship-pickly.firebaseio.com',
+  projectId: 'm3ntorship-pickly',
+  storageBucket: 'm3ntorship-pickly.appspot.com',
+  messagingSenderId: '56580619370',
+  appId: '1:56580619370:web:e96021c4016eda45455e13'
 };
 var fire = firebase.initializeApp(firebaseConfig);
 
