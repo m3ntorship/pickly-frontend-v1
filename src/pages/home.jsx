9f36ef233686ce4f9c433efb78afb4440531f329
Eslam Hesham <esl2mhesham@gmail.com>
2020-10-12 21:33:20 +0200

    Redirct user to login form

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../context/userContext';
+import { useHistory } from 'react-router-dom';
 
 export const Home = () => {
+  const user = useContext(UserContext);
+
+  const history = useHistory();
+  if (!user) {
+    history.push('/login');
+  }
+
   return (
     <div className="h-screen bg-c800">
       <h1>Hello, This is the home page</h1>
--- src/pages/notifications.jsx
+++ src/pages/notifications.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../context/userContext';
+import { useHistory } from 'react-router-dom';
 
 export const Notifications = () => {
+  const user = useContext(UserContext);
+
+  const history = useHistory();
+  if (!user) {
+    history.push('/login');
+  }
+
   return (
     <div className="h-screen bg-c800">
       <h1>Notifications</h1>
