79fff950b50dc139ce771a5d946a7daa95aae768
AlaaSaedy <alaa.saedy.eg@gmail.com>
2020-10-13 15:52:50 +0200

    creating post section

--- src/components/PostSection/PostSection.stories.js
+++ src/components/PostSection/PostSection.stories.js
@@ -20,9 +20,11 @@ PostSectionCase.args = {
     'https://images.unsplash.com/photo-1602432058111-9ee74ac7b683?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
   bgImageTwo:
     'https://images.unsplash.com/photo-1602341612227-1612891401ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-  divWidth: '300px',
+  divWidth: '50%',
   divHeight: '400px',
   smallRound: true,
+  bgColor: 'white',
   pickIcon: 'http://www.svgshare.com/i/QUe.svg',
-  searchIcon: 'https://i.imgur.com/inlBQ6A.png'
+  searchIcon: 'https://i.imgur.com/inlBQ6A.png',
+  options: [0]
 };
--- src/components/PostSection/index.jsx
+++ src/components/PostSection/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageWithSideTitle from '../ImageWithSideTitle/index';
 import { ReusableDiv } from '../DivWithCenterdChildren/index';
-import { Icon } from '../Navbar-icons';
+import PopUp from '../OptionsBtn/index';
 
 const PostSection = ({
   title,
@@ -10,46 +10,73 @@ const PostSection = ({
   paragraph,
   bgImageOne,
   bgImageTwo,
-  divWidth,
   divHeight,
   smallRound,
+  bgColor,
   pickIcon,
-  searchIcon
+  searchIcon,
+  options
 }) => {
+  const [iconDisplay, setIconDisplay] = useState('');
+  const viewWidth = document.body.clientWidth;
+  const orIconSm = { left: '46%' };
+  const orIconLg = { left: '47.7%' };
+
+  if (viewWidth <= 640) {
+    divHeight = 150;
+  }
+
   return (
-    <div>
+    <div className="container">
       <ImageWithSideTitle title={title} subTitle={subTitle} imgURL={imgURL} />
       <p className="my-5 text-sm font-regular">{paragraph}</p>
-      <div className="flex flex-col md:flex-row">
-        <div className="relative flex">
-          <ReusableDiv
-            bgImage={bgImageOne}
-            divHeight={divHeight}
-            divWidth={divWidth}
-            smallRound={smallRound}
-          />
-          <div className="absolute z-50 h-8 w-8 right-0 mt-48 -mr-4 bg-white my-auto rounded-full text-xs hidden lg:flex items-center justify-center">
-            OR
-          </div>
+      <div className="w-full grid grid-cols-2 gap-1 relative">
+        <ReusableDiv
+          bgImage={bgImageOne}
+          divHeight={divHeight}
+          smallRound={smallRound}
+        />
+        <div
+          style={viewWidth <= 460 ? orIconSm : orIconLg}
+          className="absolute bg-white flex items-center self-center justify-center rounded-full h-8 w-8 text-xs"
+        >
+          OR
         </div>
+
         <ReusableDiv
           bgImage={bgImageTwo}
           divHeight={divHeight}
-          divWidth={divWidth}
           smallRound={smallRound}
         >
-          <div className="flex flex-col items-center justify-center">
-            <div className="bg-white rounded-full h-10 w-10">
-              <ImageWithSideTitle iconURL={pickIcon} />
-            </div>
-            <div className="flex items-center mt-2">
-              <div className="bg-white rounded-full h-10 w-10">
-                <ImageWithSideTitle iconURL={searchIcon} />
-              </div>
-              <div className="mx-1 mt-"></div>
-              <div className="bg-white rounded-full h-10 w-10">
-                <ImageWithSideTitle iconURL={searchIcon} />
-              </div>
+          <div className="flex flex-col items-center">
+            <ReusableDiv
+              bgColor={iconDisplay}
+              fullRound={true}
+              divWidth="60px"
+              divHeight="60px"
+              clickFunction={() => setIconDisplay(bgColor)}
+            >
+              <img src={pickIcon} alt="" style={{ width: '35px' }} />
+            </ReusableDiv>
+
+            <div className="flex mt-2">
+              <ReusableDiv
+                bgColor="white"
+                fullRound={true}
+                divHeight="28px"
+                divWidth="28px"
+              >
+                <img src={searchIcon} alt="" style={{ width: '13px' }} />
+              </ReusableDiv>
+              <span className="mx-2"></span>
+              <ReusableDiv
+                bgColor="white"
+                fullRound={true}
+                divHeight="28px"
+                divWidth="28px"
+              >
+                <PopUp appearOn="click" options={options} />
+              </ReusableDiv>
             </div>
           </div>
         </ReusableDiv>
@@ -59,3 +86,26 @@ const PostSection = ({
 };
 
 export default PostSection;
+
+{
+  /* <div
+            className={`flex flex-col justify-center items-center ${iconDisplay} `}
+            onMouseOver={() => {
+              setIconDisplay('opacity-100');
+            }}
+            onMouseOut={() => setIconDisplay('opacity-0')}
+          >
+            <div className="h-10 w-10 rounded-full flex items-center justify-center bg-white">
+              <img src={pickIcon} alt="" style={{ width: '13px' }} />
+            </div>
+            <div className="flex mt-2">
+              <div className="h-10 w-10 rounded-full flex items-center justify-center bg-white">
+                <img src={searchIcon} alt="" style={{ width: '13px' }} />
+              </div>
+              <span className="mx-1"></span>
+              <div className="h-10 w-10 rounded-full flex items-center justify-center bg-white">
+                <PopUp appearOn="click" options={options} />
+              </div>
+            </div>
+          </div> */
+}
