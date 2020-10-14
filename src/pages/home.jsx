a9b71dbc295165efe0365d0030c07de66eeb0ccd
MAlngaawy <m.alngaawy@gmail.com>
2020-10-14 19:22:54 +0200

    add popup width style and test it in the localhost 3000

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { PostSomething } from '../components/PostSomething';
 
 export const Home = () => {
   return (
     <div className="h-screen bg-c800">
-      <h1>Hello, This is the home page</h1>
+      <PostSomething />
     </div>
   );
 };
