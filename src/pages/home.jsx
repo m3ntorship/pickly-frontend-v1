1f01e6effbae46eec90445daab16326f4f39959a
AlaaSaedy <alaa.saedy.eg@gmail.com>
2020-10-19 17:57:17 +0200

    - Add Create Post Button  To Homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,8 @@ import PostSection from '../components/PostSection';
 import { PostSomething } from '../components/PostSomething';
 import { create } from 'axios';
 import PostLoader from '../components/LoadingComponents/PostLoader';
+import { Button, BUTTON_OPTIONS } from '../components/Button';
+import { Heading, HEADING_OPTIONS } from '../components/Heading';
 
 const API = create({
   baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001/'
@@ -36,7 +38,31 @@ export const Home = () => {
   return (
     <div className="bg-c900 py-6">
       <div className="container">
-        <PostSomething />
+        <Button
+          shadow={true}
+          isRounded={true}
+          color={BUTTON_OPTIONS.BACKGROUND_COLOR.PrimaryBlack}
+          className="flex flex-shrink py-3 px-4 my-6"
+        >
+          <i className="mr-3 hidden sm:block h-8 w-8">
+            <img
+              src={
+                'https://www.flaticon.com/svg/static/icons/svg/1837/1837512.svg'
+              }
+              alt="choose one"
+            ></img>
+          </i>
+          <Heading
+            textAlign={HEADING_OPTIONS.TEXT_ALIGN.LEFT}
+            fontSize={HEADING_OPTIONS.FONT_SIZE.XSMALL}
+            fontWeight={HEADING_OPTIONS.FONT_WEIGHT.SEMIBOLD}
+            lineHeight={HEADING_OPTIONS.LINE_HEIGHT.LOOSE}
+            className="sm:text-base"
+          >
+            Create Post
+          </Heading>
+        </Button>
+
         {loading && <PostLoader />}
         {error && <ErrorComponent />}
         {data &&
@@ -75,11 +101,11 @@ export const Home = () => {
 const ErrorComponent = () => {
   return (
     <div
-      class="bg-c900 border border-c200 text-c200 px-4 py-3 rounded relative"
+      className="bg-c900 border border-c200 text-c200 px-4 py-3 rounded relative"
       role="alert"
     >
-      <strong class="font-bold">Sorry!</strong>
-      <span class="block sm:inline ml-2">Can't find your data.</span>
+      <strong className="font-bold">Sorry!</strong>
+      <span className="block sm:inline ml-2">Can't find your data.</span>
     </div>
   );
 };
