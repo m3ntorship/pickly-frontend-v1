c4a8e7f2ba57e53e81e30a796989e1c90ee75e02
AlaaSaedy <alaa.saedy.eg@gmail.com>
2020-10-19 18:05:41 +0200

    Add router to button to direct user to New Post route

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/userContext';
 import PostSection from '../components/PostSection';
-import { PostSomething } from '../components/PostSomething';
 import { create } from 'axios';
 import PostLoader from '../components/LoadingComponents/PostLoader';
 import { Button, BUTTON_OPTIONS } from '../components/Button';
 import { Heading, HEADING_OPTIONS } from '../components/Heading';
+import { NavLink } from 'react-router-dom';
 
 const API = create({
   baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001/'
@@ -38,30 +38,32 @@ export const Home = () => {
   return (
     <div className="bg-c900 py-6">
       <div className="container">
-        <Button
-          shadow={true}
-          isRounded={true}
-          color={BUTTON_OPTIONS.BACKGROUND_COLOR.PrimaryBlack}
-          className="flex flex-shrink py-3 px-4 my-6"
-        >
-          <i className="mr-3 hidden sm:block h-8 w-8">
-            <img
-              src={
-                'https://www.flaticon.com/svg/static/icons/svg/1837/1837512.svg'
-              }
-              alt="choose one"
-            ></img>
-          </i>
-          <Heading
-            textAlign={HEADING_OPTIONS.TEXT_ALIGN.LEFT}
-            fontSize={HEADING_OPTIONS.FONT_SIZE.XSMALL}
-            fontWeight={HEADING_OPTIONS.FONT_WEIGHT.SEMIBOLD}
-            lineHeight={HEADING_OPTIONS.LINE_HEIGHT.LOOSE}
-            className="sm:text-base"
+        <NavLink exact to="/post">
+          <Button
+            shadow={true}
+            isRounded={true}
+            color={BUTTON_OPTIONS.BACKGROUND_COLOR.PrimaryBlack}
+            className="flex flex-shrink py-3 px-4 my-6"
           >
-            Create Post
-          </Heading>
-        </Button>
+            <i className="mr-3 hidden sm:block h-8 w-8">
+              <img
+                src={
+                  'https://www.flaticon.com/svg/static/icons/svg/1837/1837512.svg'
+                }
+                alt="choose one"
+              ></img>
+            </i>
+            <Heading
+              textAlign={HEADING_OPTIONS.TEXT_ALIGN.LEFT}
+              fontSize={HEADING_OPTIONS.FONT_SIZE.XSMALL}
+              fontWeight={HEADING_OPTIONS.FONT_WEIGHT.SEMIBOLD}
+              lineHeight={HEADING_OPTIONS.LINE_HEIGHT.LOOSE}
+              className="sm:text-base"
+            >
+              Create Post
+            </Heading>
+          </Button>
+        </NavLink>
 
         {loading && <PostLoader />}
         {error && <ErrorComponent />}
