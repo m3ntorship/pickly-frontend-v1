7696c8ceb431dee995ac69c056b0cbb208f0adc4
MAlngaawy <m.alngaawy@gmail.com>
2020-10-28 01:14:55 +0200

    handle upload section the right way

--- src/components/UploadSection/uploadSection.stories.js
+++ src/components/UploadSection/uploadSection.stories.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { UploadSectionStory } from './uploadSection';
+import { UploadSection } from './';
 
 export default {
   title: 'sections/upload section',
-  component: UploadSectionStory
+  component: UploadSection
 };
 
 export const CropImageSection = () => {
-  return <UploadSectionStory />;
+  return (
+    <UploadSection userImage="https://prestigeportraits.com/wp-content/uploads/2015/06/PortraitGallery_2Block-350x200-1-273x200.jpg" />
+  );
 };
