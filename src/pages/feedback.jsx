97f8373cdea3152a5148fb0c5aa4d5dadf453fad
Mu'taz <mutazalaasalah@gmail.com>
2020-11-03 13:06:37 +0200

    resolve feedback route

--- src/pages/feedback.jsx
+++ src/pages/feedback.jsx
@@ -1,21 +1,28 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/userContext';
 import { useHistory } from 'react-router-dom';
+
 export const Feedback = () => {
   const { user } = useContext(UserContext);
   const history = useHistory();
-  const [value, setValue] = useState(null);
+  const [value, setValue] = useState({
+    val:"",msgError:""
+  });
   useEffect(() => {
     if (!user) {
       history.push('/login');
     }
   });
   const handleValidationInput = e => {
-    setValue(e.target.value);
+    if(e.target.value===""){
+      setValue({val:value.val,msgError:"You should put fkgfjg"})
+    }else if(!value.val&&value.msgError){
+    setValue({val:e.target.value,msgError:""});
+    }
   };
   return (
-    <div className="relative w-screen bg-white pt-4 lg:pt-16 pb-2 flex">
-      <div className="absolute ml-8 hidden lg:block">
+    <div className="lg:relative lg:w-full bg-c900 pt-4 lg:pt-16 pb-2 flex">
+      <div className="absolute lg:ml-8 hidden lg:block z-50">
         <svg
           width="111"
           height="222"
@@ -45,8 +52,8 @@ export const Feedback = () => {
           />
         </svg>
       </div>
-      <div className="w-screen absolute">
-      <div className="  nav__container capitalize lg:grid lg:grid-cols-2 inline-block">
+      <div className="w-full lg:absolute bg-c900 z-0 flex justify-center">
+      <div className="w-full  nav__container capitalize lg:grid lg:grid-cols-2 inline-block">
         <div className="mt-16 lg:mt-2">
           <h1 className="text-xxlg font-xbold">
             Help us with
@@ -56,25 +63,25 @@ export const Feedback = () => {
           <p className="text-c500 mt-4">
             At elit leo dignissim volutpat sit.et amet enim, porttitor felis
           </p>
-          <form className="mt-12" method="post">
+          <form className="mt-12 mb-12" method="post">
             <select className="w-full h-16 shadow-background text-c500 rounded-lg pl-4 text-md block">
-              <option value="category"  defaultValue>
+              <option className="pt-1" value="category"  defaultValue>
                 Category
               </option>
-              <option value="problem1">Problem.1</option>
-              <option value="problem2">problem.2</option>
-              <option value="problem3">problem.3</option>
+              <option className="pt-1" value="problem1">Problem.1</option>
+              <option className="pt-1" value="problem2">Problem.2</option>
+              <option className="pt-1" value="problem3">Problem.3</option>
             </select>
             <textarea
               onChange={handleValidationInput}
-              defaultValue={value}
+              defaultValue={value.val}
               className="mt-4 w-full h-16 shadow-background text-c500 rounded-lg pl-4 pt-4 resize-none block"
               type="text"
               placeholder="Problem"
               required
             ></textarea>
-            <p className={`text-c200 ${value ? 'hidden' : 'block'}`}>
-              you should put your problem
+            <p className="text-c200">
+              {value.msgError}
             </p>
             <button
               className={`mt-6 w-full h-12 lg:w-1/2 rounded-lg ${
