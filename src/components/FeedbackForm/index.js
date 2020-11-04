96b60e4268494afcce0471e08284f9cc4dfb8b38
Mu'taz <mutazalaasalah@gmail.com>
2020-11-04 14:00:41 +0200

    create custome select

---
+++ src/components/FeedbackForm/index.js
@@ -0,0 +1,61 @@
+import React,{useState} from "react"
+import { Button, BUTTON_OPTIONS } from '../Button/index';
+import icon from "./down-chevron.svg"
+
+export const FeedbackForm=()=>{
+ 
+    const [value,setVal]=useState({val:"",errorMsg:"",status:false,dropDownValue:""})
+    const handleDropDown =()=>{
+      setVal({val:value.val,errorMsg:"",status:!value.status})
+    }
+    const validationHandling=()=>{
+      console.log(value.val)
+      console.log(value.dropDownValue)
+      if(!value.val||!value.dropDownValue){
+          setVal({...value,errorMsg:"Problem field needed"})
+      }
+  }
+    const handleValueFromDropDown=(e)=>{
+      setVal({...value,status:false,dropDownValue:e.target.innerHTML})
+    }
+    return(
+        <form className="relative mb-12 bg-c900" onSubmit={(e)=>e.preventDefault()}>
+             
+              <div className="absolute w-full h-16 flex justify-end mt-4">
+                  <input type="text" defaultValue={value.dropDownValue} placeholder="Category" className="z-40 absolute w-full h-16 shadow-background text-c500 rounded-lg pl-4"/>
+                  <div onClick={handleDropDown} className="absolute w-16 h-16 shadow-background text-c500 flex justify-center items-center z-40">
+             
+                <img src={icon} alt=""/>
+              </div>
+              </div>
+              <div>
+              {value.status?<div className={`absolute shadow-xl text-c500 w-full z-30 bg-white`}>
+                <ul className=" mt-20 w-full">
+                  <li 
+                  onClick={handleValueFromDropDown}
+                  className="pt-2 pb-2 pl-4 w-full hover:bg-c900 hover:pl-0 hover:text-black">Problem1</li>
+                  <li onClick={handleValueFromDropDown} className="pt-2 pb-2 pl-4 w-full hover:bg-c900 hover:pl-0 hover:text-black">Problem2</li>
+                  <li onClick={handleValueFromDropDown} className="pt-2 pb-2 pl-4 w-full hover:bg-c900 hover:pl-0 hover:text-black">Problem3</li>
+                </ul>
+              </div>:null}
+              <textarea
+              onChange={(e)=>setVal({val:e.target.value,errorMsg:value.errorMsg})}
+                defaultValue={value.val}
+                className="absolute w-full h-16 shadow-background text-c500 rounded-lg pl-4 pt-4 resize-none block top-50 z-10"
+                type="text"
+                placeholder="Problem"
+                style={{top:"6rem"}}
+                
+              ></textarea>
+            {/* <p className="text-c200">{value.errorMsg}</p> */}
+            <div className="flex w-full h-full justify-center lg:justify-start">
+            <p className="text-c200">{value.errorMsg}</p>
+
+              <Button handleClick={validationHandling} shadow={true} isRounded={true} backgroundColor={BUTTON_OPTIONS.BACKGROUND_COLOR.Blue} color={BUTTON_OPTIONS.COLOR.White} padding={BUTTON_OPTIONS.PADDING.BIG}>
+                Send Your Feedback
+              </Button>
+              </div>
+              </div>
+            </form>
+    )
+}
--- src/pages/feedback.jsx
+++ src/pages/feedback.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/userContext';
 import { useHistory } from 'react-router-dom';
+import { FeedbackForm } from "../components/FeedbackForm/index"
+import { Button, BUTTON_OPTIONS } from "../components/Button/index";
 
 export const Feedback = () => {
   const { user } = useContext(UserContext);
@@ -14,21 +16,14 @@ export const Feedback = () => {
       history.push('/login');
     }
   });
-  const handleValidationInput = e => {
-    if (e.target.value === '') {
-      setValue({ val: value.val, msgError: 'You should put fkgfjg' });
-    } else if (!value.val && value.msgError) {
-      setValue({ val: e.target.value, msgError: '' });
-    }
-  };
   return (
-    <div className="lg:relative lg:w-full bg-c900 pt-4 lg:pt-16 pb-2 flex">
+    <div className="lg:relative lg:w-full h-screen bg-c900 pt-4 lg:pt-16 pb-2 flex">
       <div className="absolute lg:ml-8 hidden lg:block z-50">
         <img src="https://res.cloudinary.com/dhwuqox2w/image/upload/v1604419570/stars_i0_ordxth.svg" alt=""/>
       </div>
-      <div className="w-full lg:absolute bg-c900 z-0 flex justify-center">
+      <div className="w-full lg:absolute bg-c900 z-0 flex justify-center items-start">
         <div className="w-full  nav__container capitalize lg:grid lg:grid-cols-2 inline-block">
-          <div className="mt-16 lg:mt-2">
+          <div>
             <h1 className="text-xxlg font-xbold">
               Help us with
               <br />
@@ -37,43 +32,10 @@ export const Feedback = () => {
             <p className="text-c500 mt-4">
               At elit leo dignissim volutpat sit.et amet enim, porttitor felis
             </p>
-            <form className="mt-12 mb-12" method="post">
-              <select className="w-full h-16 shadow-background text-c500 rounded-lg pl-4 text-md block">
-                <option className="pt-1" value="category" defaultValue style={{
-                  padding:"16px"
-                }}>
-                  Category
-                </option>
-                <option className="pt-1" value="problem1">
-                  Problem.1
-                </option>
-                <option className="pt-1" value="problem2">
-                  Problem.2
-                </option>
-                <option className="pt-1" value="problem3">
-                  Problem.3
-                </option>
-              </select>
-              <textarea
-                onChange={handleValidationInput}
-                defaultValue={value.val}
-                className="mt-4 w-full h-16 shadow-background text-c500 rounded-lg pl-4 pt-4 resize-none block"
-                type="text"
-                placeholder="Problem"
-                required
-              ></textarea>
-              <p className="text-c200">{value.msgError}</p>
-              <button
-                className={`mt-6 w-full h-12 lg:w-1/2 rounded-lg ${
-                  !value.val ? 'bg-c500' : 'bg-c1100'
-                } text-white font-bold`}
-              >
-                Send Your feedback
-              </button>
-            </form>
+            <FeedbackForm />
           </div>
           <div className="hidden lg:block">
-            <img src="https://res.cloudinary.com/dhwuqox2w/image/upload/v1604419573/flame-1266_1_ttq8bp.svg" className="w-full h-full" alt="" />
+            <img src="https://res.cloudinary.com/dhwuqox2w/image/upload/v1604419573/flame-1266_1_ttq8bp.svg" className="w-full" alt="" />
           </div>
         </div>
       </div>
