504c15f5986ac1d242d2f58125e307349a10b7ce
eslamheshamm <esl2mhesham@gmail.com>
2020-11-10 20:57:18 +0200

    loop over notifications data and display it to the user, added the time function, added function open post link when 'onclick ' notifiacation,

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -9,7 +9,31 @@ export const Notification = ({
   postUrl,
   seen
 }) => {
-  const postCaptionSliced = postCaption.slice(0, 8);
+  const postCaptionSliced = postCaption.slice(0, 12);
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  const date = new Date(postTime);
+  const monthName = months[date.getMonth()];
+  const dayIndex = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formatedHours = ((hours + 11) % 12) + 1;
+  const onHandleClick = () => {
+    window.open(`${window.location.href}posts/${postUrl}`, '_self');
+  };
   return (
     <div
       className={cn(
@@ -17,7 +41,9 @@ export const Notification = ({
           ? 'p-4 sm:p-8 shadow rounded-lg bg-white my-2 cursor-pointer'
           : 'p-4 sm:p-8 shadow-md rounded-lg bg-c100_op-10 bg-opacity-100 my-2 cursor-pointer'
       )}
-      postUrl={postUrl}
+      onClick={() => {
+        onHandleClick();
+      }}
     >
       <div className="flex items-center">
         <figure>
@@ -32,41 +58,48 @@ export const Notification = ({
             <span className="font-bold">{`${userName} `}</span>
             voted on your post {`"${postCaptionSliced}"`}
           </p>
-          <time className="block text-xs text-c500">{postTime}</time>
+          <time className="block text-xs text-c500">{`${dayIndex} ${monthName} at ${formatedHours}:${minutes} ${
+            hours > 12 ? 'PM' : 'AM'
+          }`}</time>
         </div>
       </div>
     </div>
   );
 };
-export const NotificationSection = () => {
+export const NotificationSection = ({ data }) => {
   return (
-    <div className=" h-screen ">
-      <div className="nav__container ">
-        <Heading
-          as="h1"
-          children="Notifications"
-          fontWeight={HEADING_OPTIONS.FONT_WEIGHT.SEMIBOLD}
-          fontSize={HEADING_OPTIONS.FONT_SIZE.LARGE}
-          lineHeight={HEADING_OPTIONS.LINE_HEIGHT.NORMAL}
-          textAlign={HEADING_OPTIONS.TEXT_ALIGN.LEFT}
-          textColor={HEADING_OPTIONS.FONT_COLOR.DAWNBLACK}
-          className="py-6"
-        />
-        <Notification
-          imageSrc="https://source.unsplash.com/random"
-          userName="Eslam Hesham"
-          postTime={`2 days ago`}
-          postCaption="hi there byeys"
-          seen={true}
-        />
-        <Notification
-          imageSrc="https://source.unsplash.com/random"
-          userName="Eslam Hesham"
-          postTime={`2 days ago`}
-          postCaption="hi there byeys"
-          seen={false}
-        />
-      </div>
+    <div className="nav__container ">
+      <Heading
+        as="h1"
+        children="Notifications"
+        fontWeight={HEADING_OPTIONS.FONT_WEIGHT.SEMIBOLD}
+        fontSize={HEADING_OPTIONS.FONT_SIZE.LARGE}
+        lineHeight={HEADING_OPTIONS.LINE_HEIGHT.NORMAL}
+        textAlign={HEADING_OPTIONS.TEXT_ALIGN.LEFT}
+        textColor={HEADING_OPTIONS.FONT_COLOR.DAWNBLACK}
+        className="py-6"
+      />
+      {data &&
+        data.map(({ _id, sender, createdAt, retrieved, entity }) => {
+          return (
+            <Notification
+              data={data}
+              key={_id}
+              _id={_id}
+              postCaption={entity && entity.caption ? entity.caption : 'Error'}
+              postUrl={entity._id}
+              seen={retrieved}
+              postTime={createdAt}
+              userName={sender && sender.name}
+              // this will handle with a simple code when after clearing the users from backend
+              imageSrc={
+                sender && sender.userImage
+                  ? sender.userImage
+                  : 'https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg'
+              }
+            />
+          );
+        })}
     </div>
   );
 };
