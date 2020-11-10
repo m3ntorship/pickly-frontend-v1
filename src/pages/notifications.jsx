b6c136b7ea12243172a91e4f6b11e078414c7d37
eslamheshamm <esl2mhesham@gmail.com>
2020-11-10 21:12:24 +0200

    getting the notification data and pass it to the notification section & show error to the user if something wrong happened

--- src/pages/notifications.jsx
+++ src/pages/notifications.jsx
@@ -1,9 +1,12 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/userContext';
+import { PICKLY } from '../apis/clients/pickly';
 import { useHistory } from 'react-router-dom';
 import { NotificationSection } from '../components/Notifications/index';
-export const Notifications = ({ postDate }) => {
+export const Notifications = () => {
   const { user } = useContext(UserContext);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
@@ -12,9 +15,37 @@ export const Notifications = ({ postDate }) => {
     }
   });
 
+  // This useEffect() for fetching data when the route load
+  useEffect(() => {
+    PICKLY.getAllNotifications()
+      .then(({ data }) => {
+        setData(data.notifications);
+      })
+      .catch(err => {
+        console.log(err);
+        setError(true);
+      });
+  }, [token]);
+  useEffect(() => {}, [data]);
+
+  const ErrorComponent = () => {
+    return (
+      <div
+        className="bg-c900 border border-c200 text-c200 px-4 py-3 rounded relative"
+        role="alert"
+      >
+        <strong className="font-bold">Sorry!</strong>
+        <span className="block sm:inline ml-2">Can't find your data.</span>
+      </div>
+    );
+  };
+
   return (
-    <div className=" h-screen ">
-      <NotificationSection />
+    <div className="h-screen ">
+      <NotificationSection data={data} />
+      <div className="nav__container mx-auto">
+        {error && <ErrorComponent />}
+      </div>
     </div>
   );
 };
